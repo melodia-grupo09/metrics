759ba5be7dbad92a7eee2416ea95ef808d3154f3
ignacioregazzoli <ignacioregazzoli@gmail.com>
2025-09-18 15:49:59 -0300

    Add Rabbit micro service

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 
 async function bootstrap() {
   const logger = new Logger('Bootstrap');
@@ -9,6 +10,20 @@ async function bootstrap() {
     logger: ['error', 'warn', 'log', 'debug', 'verbose'],
   });
 
+  const microservice =
+    await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+      transport: Transport.RMQ,
+      options: {
+        urls: [process.env.RABBITMQ_URL || 'amqp://localhost:5672'],
+        queue: 'metrics_queue',
+        queueOptions: { durable: true },
+        exchange: 'metrics_exchange',
+        exchangeType: 'topic',
+        noAck: false,
+      },
+    });
+
+  await microservice.listen();
   await app.listen(process.env.PORT ?? 3000);
   logger.log(`Application listening on port ${process.env.PORT ?? 3000}`);
 }
