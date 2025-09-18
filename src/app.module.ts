ed6fb8265bb136e1f71be4a6fbedf51bf475116f
ignacioregazzoli <ignacioregazzoli@gmail.com>
2025-09-18 15:41:37 -0300

    Add database to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RabbitModule } from './rabbit/rabbit.module';
 import { MetricsModule } from './metrics/metrics.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [RabbitModule, MetricsModule],
+  imports: [RabbitModule, MetricsModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
