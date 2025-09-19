456aed656f438de8466d373300f6723aed176cc4
ignacioregazzoli <ignacioregazzoli@gmail.com>
2025-09-19 11:06:19 -0300

    Remove unnecesary Rabbit configuration

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
-import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 
 async function bootstrap() {
   const logger = new Logger('Bootstrap');
@@ -10,20 +9,6 @@ async function bootstrap() {
     logger: ['error', 'warn', 'log', 'debug', 'verbose'],
   });
 
-  const microservice =
-    await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
-      transport: Transport.RMQ,
-      options: {
-        urls: [process.env.RABBITMQ_URL || 'amqp://localhost:5672'],
-        queue: 'metrics_queue',
-        queueOptions: { durable: true },
-        exchange: 'metrics_exchange',
-        exchangeType: 'topic',
-        noAck: false,
-      },
-    });
-
-  await microservice.listen();
   await app.listen(process.env.PORT ?? 3000);
   logger.log(`Application listening on port ${process.env.PORT ?? 3000}`);
 }
